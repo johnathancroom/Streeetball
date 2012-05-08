d3fcbad0d1affbe642fae42981eb705f133d6b35
Johnathan Croom <johnathancroom@gmail.com>
2012-05-08 13:00:08 -0700

    Started Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Streeetball::Application.config.secret_token = 'd61db5d3448fc58b1fe41b45284a8de45145de37b2805132110b1073a7943497ff8fa6acf7c74d4370b3137fdd913a37ff1997209fecf64e48ebf635a07a4fef'
