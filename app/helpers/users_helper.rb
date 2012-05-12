7c555a74d5bf7d29aee663ab0fc710583c8e1f79
Johnathan Croom <johnathancroom@gmail.com>
2012-05-12 06:53:23 -0700

    Top bar styling
    - Fixed logged in state
    - Mini profile pic added
    
    - Changed profile_image_tag to accept entire user hash rather than URL

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def profile_image_tag(source, options = {})
-    if !source.nil?
-      s3_image_tag source, options
+    if !source.image_url.nil?
+      s3_image_tag source.image_url, options
     else
       # Default profile image
       s3_image_tag 'default_profile_image.jpeg', options
