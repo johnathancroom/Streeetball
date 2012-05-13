d6d4768d8d8b2436b972e24dcc148bc3d9516f5c
Johnathan Croom <johnathancroom@gmail.com>
2012-05-13 14:12:23 -0700

    Profile page blank social network issue fixed
    Without this commit, the social network links would still show up even
    if the username was defined as an empty string. This fixed that problem.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,13 +9,13 @@ module UsersHelper
   end
   
   def link_to_dribbble(user, options= {})
-    if user.dribbble_username
+    if user.dribbble_username && !user.dribbble_username.empty?
       link_to user.dribbble_username, "http://dribbble.com/#{user.dribbble_username}", options
     end
   end
   
   def link_to_twitter(user, options= {})
-    if user.twitter_username
+    if user.twitter_username && !user.twitter_username.empty?
       link_to user.twitter_username, "http://twitter.com/#{user.twitter_username}", options
     end
   end
