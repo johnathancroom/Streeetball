ffed567e2805d48f2125fc5959f4eed79a88f4f5
Johnathan Croom <johnathancroom@gmail.com>
2012-05-25 21:51:30 -0700

    Fixed quotation problem for pos

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.where('image_file_name != ""').reverse
+    @posts = Post.where("image_file_name != ''").reverse
 
     respond_to do |format|
       format.html # index.html.erb
