f3ba954852c052db52ab385724753ee08d366cfa
Johnathan Croom <johnathancroom@gmail.com>
2012-05-28 12:50:43 -0700

    Open dribbble and twitter URLs in new windows

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,14 @@
 module UsersHelper
-  def link_to_dribbble(user, options= {})
+  def link_to_dribbble(user, options = {})
     if user.dribbble_username && !user.dribbble_username.empty?
+      options[:target] = '_blank'
       link_to ' Draft me', "http://dribbble.com/#{user.dribbble_username}", options
     end
   end
   
-  def link_to_twitter(user, options= {})
+  def link_to_twitter(user, options = {})
     if user.twitter_username && !user.twitter_username.empty?
+      options[:target] = '_blank'
       link_to user.twitter_username, "http://twitter.com/#{user.twitter_username}", options
     end
   end
