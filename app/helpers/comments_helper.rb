12460f41df1a74acd3baa3c99589ccc2f690970e
Johnathan Croom <johnathancroom@gmail.com>
2012-05-28 22:49:23 -0700

    Added @mentioning in comments BLAM-O!
    Closed #54

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,13 @@
 module CommentsHelper
+  # Add links to profiles on mentions in comments
+  def parse_mentions(blurb)
+    raw blurb.gsub(/(?<user>@([A-Z]|[a-z])+)/) { # Search for @name format
+      @username = $1
+      if @user = User.where('lower(username) = lower(?)', @username.gsub(/@/, '')).first # Check if username exists
+        link_to "@#{@user.username}", user_path(@user) # Return link to username
+      else
+        @username
+      end
+    }
+  end
 end
