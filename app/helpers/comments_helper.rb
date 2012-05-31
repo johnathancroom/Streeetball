64aa77d661c6ef558de972790d0a0e2f6ae6379e
Johnathan Croom <johnathancroom@gmail.com>
2012-05-30 18:11:37 -0700

    Improved comment mentions regex
    Includes support for dashes and underscores
    Thanks to http://leaverou.github.com/regexplained/

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,7 +1,7 @@
 module CommentsHelper
   # Add links to profiles on mentions in comments
   def parse_mentions(blurb)
-    raw blurb.gsub(/(?<user>@([A-Z]|[a-z])+)/) { # Search for @name format
+    raw blurb.gsub(/(?<user>@(\w|-|_)+)/) { # Search for @name format
       @username = $1
       if @user = User.where('lower(username) = lower(?)', @username.gsub(/@/, '')).first # Check if username exists
         link_to "@#{@user.username}", user_path(@user) # Return link to username
