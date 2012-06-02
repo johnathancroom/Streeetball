1dec8bf87945cccea51da46bcac952c5f68732cd
Johnathan Croom <johnathancroom@gmail.com>
2012-06-02 09:34:29 -0700

    Redirect to last page after logging in
    - Fixed #33

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     if @user = User.authenticate(params[:user], params[:password])
       if @user.email_confirmed
         cookies.permanent[:auth_token] = @user.auth_token
-        redirect_to root_url, :notice => 'Signed in!'
+        redirect_to params[:referrer] || root_url, :notice => 'Signed in!' # Redirect to referrer (set in login form), default to root_url if referrer doesn't exist
       else
         render 'email_confirmations/show'
       end
